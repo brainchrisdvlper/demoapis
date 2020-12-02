6ac1a7a69b53a7d7ab9fb6c803a5708c6fe71b35
Carlos Sanchez Bravo <brainchrisdvlper@MacBook-Pro-de-Carlos.local>
2020-12-01 22:07:49 -0300

    integracion con mongo

--- api/index.js
+++ api/index.js
@@ -1,4 +1,27 @@
-module.exports = (req, res) => {
+/* module.exports = (req, res) => {
     const { name = 'World' } = req.query
     res.status(200).send(`Hello ${name}!`)
-  }
+  } */
+
+/* module.exports = ( req,res) => {
+   res.send('Respuesta...')
+} */
+
+const express    =require('express')
+const mongoose   =require('mongoose')
+const bodyParser =require('body-parser')
+const cors       =require('cors')
+
+const app= express()
+app.use(bodyParser.json())
+app.use(cors())
+
+// conecta db mongo
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology:true})
+
+app.get('*', (req, res) => {
+  res.send('Servicio OK')
+})
+
+module.exports = app
